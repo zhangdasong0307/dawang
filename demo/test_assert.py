99ec5a92748bc1baae596b920afbd2abdce51cf2
zhangdasong0307 <451471253@qq.com>
2019-11-14 15:25:01 +0800

    第9次

--- demo/test_assert.py
+++ demo/test_assert.py
@@ -10,3 +10,12 @@ def test_text(driver):
     print(text)
     assert "这是一条消息" in text
     sleep(5)
+
+def test_page_source(driver):
+    driver.get("http://ui.yansl.com/")
+    driver.find_element_by_xpath("//*[@id='app']/section/section/aside/ul/li[3]/div").click()
+    driver.find_element_by_xpath("//li[contains(text(),'消息提示')]").click()
+    source=driver.page_source
+    print(source)
+    assert "手工关闭提示" in source
+    sleep(5)
