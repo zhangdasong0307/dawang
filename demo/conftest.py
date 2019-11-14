507829b200d5456fd69bc1123ce18ccbbd82734d
zhangdasong0307 <451471253@qq.com>
2019-11-14 18:02:18 +0800

    第12次

--- demo/conftest.py
+++ demo/conftest.py
@@ -1,22 +1,23 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
 import os
-from time import sleep
 
 import pytest
+
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 
-
-
-#专门用于test_allure
-
+#上课的conftest，用不着allure，所以标题用，除了test_allure不能用
 @pytest.fixture(scope='session')
 def driver():
-    # 打开浏览器
-    driver = webdriver.Chrome(os.path.join(os.path.dirname(os.path.abspath(__file__)),'../chrome-78/chromedriver.exe'))
-    # 调整浏览器窗口大小
-    driver.maximize_window()
-    driver.implicitly_wait(10) # 设置等待时长5秒
-    # 浏览器全局设置，一般在浏览器启动之后设置一次，终生有效
-    yield driver
-    driver.quit()
+    driver=webdriver.Chrome(os.path.join(os.path.dirname(os.path.abspath(__file__)),'../chrome-78/chromedriver.exe'))      #打开谷歌浏览器
+    #调整浏览器大小最大化
+    #driver.maximize_window()
+
+    #隐士等待
+    driver.implicitly_wait(5) #隐式等待，设置等待时长5
+
+
+    yield driver        #之后操作一下命令
+    driver.close()      #关闭浏览器
+
+
---
+++ demo/conftest2.py
@@ -0,0 +1,22 @@
+# #!/usr/bin/env python
+# # -*- coding:utf-8 -*-
+# import os
+# from time import sleep
+#
+# import pytest
+# from selenium import webdriver
+#
+#
+#
+# #专门用于test_allure
+#
+# @pytest.fixture(scope='session')
+# def driver():
+#     # 打开浏览器
+#     driver = webdriver.Chrome(os.path.join(os.path.dirname(os.path.abspath(__file__)),'../chrome-78/chromedriver.exe'))
+#     # 调整浏览器窗口大小
+#     driver.maximize_window()
+#     driver.implicitly_wait(10) # 设置等待时长5秒
+#     # 浏览器全局设置，一般在浏览器启动之后设置一次，终生有效
+#     yield driver
+#     driver.quit()
